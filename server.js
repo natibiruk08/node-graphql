4cc87bf705109e71a0193e8a96ce81edf4a59c3a
Natnael Biruk <natnaelbiruk3@gmail.com>
2022-06-02 15:32:15 +0300

    graphql with node-js

--- server.js
+++ server.js
@@ -1,8 +1,73 @@
 const express = require("express");
 const app = express();
-const userData = require("./MOCK_DATA.json");
 const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+} = graphql;
+const userData = require("./MOCK_DATA.json");
 const { graphqlHTTP } = require("express-graphql");
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    first_name: { type: GraphQLString },
+    last_name: { type: GraphQLString },
+    email: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllUsers: {
+      type: new GraphQLList(UserType),
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args) {
+        return userData;
+      },
+    },
+  },
+});
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createUser: {
+      type: UserType,
+      args: {
+        first_name: { type: GraphQLString },
+        last_name: { type: GraphQLString },
+        email: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        userData.push({
+          id: userData.length + 1,
+          first_name: args.first_name,
+          last_name: args.last_name,
+          email: args.email,
+        });
+        return args;
+      },
+    },
+  },
+});
+
+const schema = new graphql.GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation,
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(8080, () => {
   console.log("Server is running!");
